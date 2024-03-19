2af86fe7d701f8cb013e33230b5a646be5a36fb4
Boklyn Wong <boklyn@gmail.com>
2024-03-19 10:44:34 -0400

    Adding new scenario for execution.
    hello

---
+++ cli/jobs/pipelines/mslearn-mlops/model/train.py
@@ -0,0 +1,84 @@
+# Import libraries
+
+import argparse
+import glob
+import os
+
+import pandas as pd
+import mlflow
+import mlflow.sklearn
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+# define functions
+def main(args):
+    # Log parameters and metrics using autologging
+    mlflow.sklearn.autolog()
+
+    # read data
+    df = get_csvs_df(args.training_data)
+
+    # split data
+    X_train, X_test, y_train, y_test = split_data(df)
+
+    # train model
+    train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+
+
+def get_csvs_df(path):
+    if not os.path.exists(path):
+        raise RuntimeError(f"Cannot use non-existent path provided: {path}")
+    csv_files = glob.glob(f"{path}/*.csv")
+    if not csv_files:
+        raise RuntimeError(f"No CSV files found in provided data path: {path}")
+    return pd.concat((pd.read_csv(f) for f in csv_files), sort=False)
+
+
+def split_data(in_df, test_size=0.30, random_state=0):
+    X, y = in_df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
+    
+    return X_train, X_test, y_train, y_test
+
+def train_model(reg_rate, X_train, X_test, y_train, y_test):
+    # train model
+    model = LogisticRegression(C=1/reg_rate, solver="liblinear").fit(X_train, y_train)
+
+def parse_args():
+    # setup arg parser
+    parser = argparse.ArgumentParser()
+
+    # add arguments
+    parser.add_argument("--training_data", dest='training_data',
+                        type=str)
+    parser.add_argument("--reg_rate", dest='reg_rate',
+                        type=float, default=0.01)
+
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+
+# run script
+if __name__ == "__main__":
+    # add space in logs
+    print("\n\n")
+    print("*" * 60)
+
+    # parse args
+    args = parse_args()
+
+    # run main function
+    main(args)
+
+    # add space in logs
+    print("*" * 60)
+    print("\n\n")
+
+    # Save the model
+    mlflow.sklearn.log_model(model, "logistic_regression_model")
+    
+    # End the MLflow run
+    mlflow.end_run()
+    
